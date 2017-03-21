e4b340941047e9d95a65a9f3883ddaf6e726499a
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-03-21 13:35:14 +0100

    Split the qt variable in three for Debian

--- boilerplate/premake4.lua
+++ boilerplate/premake4.lua
@@ -11,19 +11,19 @@ solution 'myProject'
         files {'source/**.hpp', 'source/**.cpp'}
 
         -- Run moc and link to the Qt library (required only when using the Chameleon library displays)
-        -- Edit the variable 'mocFiles' to add or remove Chameleon components
-        -- Edit the qtPath variable to match your Qt version
-        local mocFiles = {
+        local mocFiles = { -- must contain Chameleon components paths
             '/usr/local/include/chameleon/backgroundCleaner.hpp',
             '/usr/local/include/chameleon/changeDetectionDisplay.hpp',
         }
-        local qtPath = '/usr/local/Cellar/qt5/5.8.0_1'
+        local mocCommand = '/usr/local/Cellar/qt5/5.8.0_1/bin/moc' -- must point to the moc executable
+        local qtIncludeDirectory = '/usr/local/Cellar/qt5/5.8.0_1/include' -- must point to the directory containing Qt's headers
+        local qtLibDirectory = '/usr/local/Cellar/qt5/5.8.0_1/lib' -- must point to the directory containing Qt's dynamic libraries
         local mocDirectory = path.getdirectory(_SCRIPT) .. '/build/moc'
         os.rmdir(mocDirectory)
         os.mkdir(mocDirectory)
         for index, mocFile in pairs(mocFiles) do
-            if os.execute(qtPath .. '/bin/moc'
-                .. ' -I' .. qtPath .. '/include/QtQml'
+            if os.execute(mocCommand
+                .. ' -I' .. qtIncludeDirectory .. '/QtQml'
                 .. ' -o ' .. mocDirectory .. '/' .. path.getbasename(mocFile) .. '.cpp '
                 .. mocFile
             ) ~= 0 then
@@ -33,10 +33,13 @@ solution 'myProject'
             print(string.char(27) .. '[32m' .. mocFile .. ' was successfully pre-compiled' .. string.char(27) .. '[0m')
         end
         files {'build/moc/**.h', 'build/moc/**.cpp', 'source/**.qml'}
-        includedirs {qtPath .. '/include', qtPath .. '/include/QtGui', qtPath .. '/include/QtQml'}
+        includedirs {qtIncludeDirectory, qtIncludeDirectory .. '/QtGui', qtIncludeDirectory .. '/QtQml'}
+        configuration 'linux'
+            libdirs {qtLibDirectory}
+            links {'Qt5Core', 'Qt5Gui', 'Qt5Qml', 'Qt5Quick', 'Qt5QuickControls'}
         configuration 'macosx'
             linkoptions {
-                '-F' .. qtPath .. '/lib',
+                '-F' .. qtLibDirectory,
                 '-framework QtCore',
                 '-framework QtGui',
                 '-framework QtQml',
