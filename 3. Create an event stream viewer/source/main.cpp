57917fd096913de687cb0150f133fb843abfb123
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-06-02 19:49:31 +0200

    Required by Qt 5.9

--- 3. Create an event stream viewer/source/main.cpp
+++ 3. Create an event stream viewer/source/main.cpp
@@ -5,6 +5,7 @@
 #include <chameleon/logarithmicDisplay.hpp>
 #include <QtGui/QGuiApplication>
 #include <QtQuick/QQuickView>
+#include <QtCore/QFileInfo>
 
 /// ExposureMeasurement holds the parameters of an exposure measurement.
 struct ExposureMeasurement {
@@ -25,10 +26,9 @@ int main(int argc, char* argv[]) {
     format.setStencilBufferSize(8);
     format.setVersion(3, 3);
     format.setProfile(QSurfaceFormat::CoreProfile);
-    QQuickView view;
+    QQuickView view(QUrl::fromLocalFile(QFileInfo("../../source/main.qml").absoluteFilePath()));
     view.setFormat(format);
     view.setResizeMode(QQuickView::SizeRootObjectToView);
-    view.setSource(QUrl::fromLocalFile("../../source/main.qml"));
     view.show();
 
     auto changeDetectionDisplay = view.rootObject()->findChild<chameleon::ChangeDetectionDisplay*>("changeDetectionDisplay");
