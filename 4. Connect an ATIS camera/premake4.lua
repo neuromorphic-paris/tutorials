306d7fcb84a2ea10bf05df853232d3bc3ddfef75
Gregor Lenz <lenz.gregor@gmail.com>
2017-09-26 10:57:07 +0200

    [ADD] tutorial based on event stream viewer that uses an ATIS

---
+++ 4. Connect an ATIS camera/premake4.lua
@@ -0,0 +1,74 @@
+solution 'eventStreamViewer'
+    configurations {'Release', 'Debug'}
+    location 'build'
+
+    project 'eventStreamViewer'
+
+        -- General settings
+        kind 'ConsoleApp'
+        language 'C++'
+        location 'build'
+        files {'source/**.hpp', 'source/**.cpp'}
+
+        -- Run moc and link to the Qt library (required only when using the Chameleon library displays)
+        local mocFiles = { -- must contain Chameleon components paths
+            '/usr/local/include/chameleon/backgroundCleaner.hpp',
+            '/usr/local/include/chameleon/changeDetectionDisplay.hpp',
+            '/usr/local/include/chameleon/logarithmicDisplay.hpp',
+        }
+        local mocCommand = '/usr/lib/x86_64-linux-gnu/qt5/bin/moc' -- must point to the moc executable
+        local qtIncludeDirectory = '/usr/include/x86_64-linux-gnu/qt5' -- must point to the directory containing Qt's headers
+        local qtLibDirectory = '/usr/lib/x86_64-linux-gnu' -- must point to the directory containing Qt's dynamic libraries
+        local mocDirectory = path.getdirectory(_SCRIPT) .. '/build/moc'
+        os.rmdir(mocDirectory)
+        os.mkdir(mocDirectory)
+        for index, mocFile in pairs(mocFiles) do
+            if os.execute(mocCommand
+                .. ' -I\'' .. qtIncludeDirectory .. '/QtQml\''
+                .. ' -o \'' .. mocDirectory .. '/' .. path.getbasename(mocFile) .. '.cpp\''
+                .. ' \''.. mocFile .. '\''
+            ) ~= 0 then
+                print(string.char(27) .. '[31mPre-compiling ' .. mocFile .. ' failed' .. string.char(27) .. '[0m')
+                os.exit(1)
+            end
+            print(string.char(27) .. '[32m' .. mocFile .. ' was successfully pre-compiled' .. string.char(27) .. '[0m')
+        end
+        files {mocDirectory .. '/**.h', mocDirectory .. '/**.cpp', 'source/**.qml'}
+        includedirs {qtIncludeDirectory, qtIncludeDirectory .. '/QtQml'}
+        configuration 'linux'
+            libdirs {qtLibDirectory}
+            links {'Qt5Core', 'Qt5Gui', 'Qt5Qml', 'Qt5Quick'}
+            buildoptions {'-fPIC'}
+        configuration 'macosx'
+            linkoptions {
+                '-F' .. qtLibDirectory,
+                '-framework QtCore',
+                '-framework QtGui',
+                '-framework QtQml',
+                '-framework QtQuick',
+                '-framework QtQuickControls2',
+            }
+        configuration {}
+
+        -- Declare the configurations
+        configuration 'Release'
+            targetdir 'build/Release'
+            defines {'NDEBUG'}
+            flags {'OptimizeSpeed'}
+        configuration 'Debug'
+            targetdir 'build/Debug'
+            defines {'DEBUG'}
+            flags {'Symbols'}
+
+        -- Linux specific settings
+        configuration 'linux'
+            buildoptions {'-std=c++11'}
+            linkoptions {'-std=c++11'}
+            includedirs {'/usr/local/include'}
+            links {'pthread', 'opalkellyfrontpanel'}
+
+        -- Mac OS X specific settings
+        configuration 'macosx'
+            buildoptions {'-std=c++11', '-stdlib=libc++'}
+            linkoptions {'-std=c++11', '-stdlib=libc++'}
+            includedirs {'/usr/local/include'}
---
+++ 4. Connect an ATIS camera/source/main.cpp
@@ -0,0 +1,65 @@
+#include <QtCore/QFileInfo>
+#include <QtGui/QGuiApplication>
+#include <QtQuick/QQuickView>
+#include <chameleon/backgroundCleaner.hpp>
+#include <chameleon/changeDetectionDisplay.hpp>
+#include <chameleon/logarithmicDisplay.hpp>
+#include <opalKellyAtisSepia.hpp>
+#include <sepia.hpp>
+#include <tarsier/stitch.hpp>
+
+/// ExposureMeasurement holds the parameters of an exposure measurement.
+struct ExposureMeasurement {
+  std::size_t x;
+  std::size_t y;
+  uint64_t timeDelta;
+};
+
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+
+  qmlRegisterType<chameleon::BackgroundCleaner>("BackgroundCleaner", 1, 0,
+                                                "BackgroundCleaner");
+  qmlRegisterType<chameleon::ChangeDetectionDisplay>(
+      "ChangeDetectionDisplay", 1, 0, "ChangeDetectionDisplay");
+  qmlRegisterType<chameleon::LogarithmicDisplay>("LogarithmicDisplay", 1, 0,
+                                                 "LogarithmicDisplay");
+
+  QSurfaceFormat format;
+  format.setDepthBufferSize(24);
+  format.setStencilBufferSize(8);
+  format.setVersion(3, 3);
+  format.setProfile(QSurfaceFormat::CoreProfile);
+  QQuickView view(
+      QUrl::fromLocalFile(QFileInfo("../source/main.qml").absoluteFilePath()));
+  view.setFormat(format);
+  view.setResizeMode(QQuickView::SizeRootObjectToView);
+  view.show();
+
+  auto changeDetectionDisplay =
+      view.rootObject()->findChild<chameleon::ChangeDetectionDisplay *>(
+          "changeDetectionDisplay");
+  auto logarithmicDisplay =
+      view.rootObject()->findChild<chameleon::LogarithmicDisplay *>(
+          "logarithmicDisplay");
+
+  auto camera = opalKellyAtisSepia::make_camera(
+      sepia::make_split(
+          [&](sepia::DvsEvent dvsEvent) -> void {
+            changeDetectionDisplay->push(dvsEvent);
+          },
+          tarsier::make_stitch<sepia::ThresholdCrossing, ExposureMeasurement,
+                               304, 240>(
+              [](sepia::ThresholdCrossing secondThresholdCrossing,
+                 uint64_t timeDelta) -> ExposureMeasurement {
+                return ExposureMeasurement{secondThresholdCrossing.x,
+                                           secondThresholdCrossing.y,
+                                           timeDelta};
+              },
+              [&](ExposureMeasurement exposureMeasurement) -> void {
+                logarithmicDisplay->push(exposureMeasurement);
+              })),
+      [](std::exception_ptr) {});
+
+  return app.exec();
+}
